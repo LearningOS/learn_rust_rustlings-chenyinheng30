695799cd1fd2fcdaea4f47a5f4ed52d8cd4e0f04
chenyinheng30 <chenyinheng30@outlook.com>
2022-10-20 18:04:43 +0800

    lab: Complete quiz1 Complete:intro, variables, functions, if

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -11,7 +11,13 @@
 // I AM NOT DONE
 
 // Put your function here!
-// fn calculate_apple_price {
+fn calculate_apple_price(apples: u32) -> u32 {
+    if apples <= 40 {
+        apples * 2
+    } else {
+        apples
+    }
+}
 
 // Don't modify this function!
 #[test]
